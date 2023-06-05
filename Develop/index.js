a38ea0164e1d635a231d018ea24b548716edc23f
daleyjones <djones_15@hotmail.com>
2023-06-05 21:52:26 +0100

    new commit

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,72 @@ const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'gitHub',
+      message: 'What is your GitHub username?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?'
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: "What is your project's name?"
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please write a short description of your project'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What command should be run to install dependencies?',
+      default: 'npm install'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What does the user need to know about using the repo?'
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'What command should be run to run tests?',
+      default: 'npm test'
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'Who should be credited for this project?',
+      default: 'None'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What kind of license should your project have?',
+      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    }
+  ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
