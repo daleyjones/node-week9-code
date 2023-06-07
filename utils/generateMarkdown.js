b087979c447a8cca56310d4e15b3d8d72fa5e12e
daleyjones <djones_15@hotmail.com>
2023-06-07 21:05:39 +0100

    last commit

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,8 @@
-
 function renderLicenseBadge(license) {
   if (!license) {
     return '';
   }
-
-
+ 
   const licenseBadges = {
     MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
     'APACHE 2.0': '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
