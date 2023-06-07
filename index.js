a25bf3512d7807b0961022e4aeb8d3cb5db93c85
daleyjones <djones_15@hotmail.com>
2023-06-07 21:04:20 +0100

    update commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
-// TODO: Create an array of questions for user input
+
 const questions = [
     {
       type: 'input',
