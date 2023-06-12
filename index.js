f499ddac7db1865e922a7fae4ee91e3118e525d9
daleyjones <djones_15@hotmail.com>
2023-06-12 18:33:07 +0100

    comments commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
+// Array of questions to prompt the user
 const questions = [
   {
     type: 'input',
@@ -55,17 +56,24 @@ const questions = [
   }
 ];
 
+// Function to write data to a file
 function writeToFile(fileName, data) {
   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
+// Function to initialize the application
 function init() {
+  // Prompt the user with questions and generate README when all responses are collected
   inquirer.prompt(questions).then((inquirerResponses) => {
     console.log('Generating README...');
 
+    // Generate markdown content based on user responses
     const markdown = generateMarkdown({ ...inquirerResponses });
+
+    // Write markdown content to README.md file
     writeToFile('README.md', markdown);
   });
 }
 
+// Call the init function to start the application
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,10 @@
+// Function to render the license badge based on the license type
 function renderLicenseBadge(license) {
   if (!license) {
     return '';
   }
 
+  // Define the license badges for different license types
   const licenseBadges = {
     MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
     'APACHE 2.0': '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
@@ -10,14 +12,17 @@ function renderLicenseBadge(license) {
     'BSD 3': '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
   };
 
+  // Return the license badge corresponding to the given license type
   return licenseBadges[license] || '';
 }
 
+// Function to render the license link based on the license type
 function renderLicenseLink(license) {
   if (!license) {
     return '';
   }
 
+  // Define the license links for different license types
   const licenseLinks = {
     MIT: 'https://opensource.org/licenses/MIT',
     'APACHE 2.0': 'https://opensource.org/licenses/Apache-2.0',
@@ -25,25 +30,33 @@ function renderLicenseLink(license) {
     'BSD 3': 'https://opensource.org/licenses/BSD-3-Clause'
   };
 
+  // Return the license link corresponding to the given license type
   return licenseLinks[license] || '';
 }
 
+// Function to render the license section for the markdown file
 function renderLicenseSection(license) {
   if (!license) {
     return '';
   }
 
+  // Get the license link based on the license type
   const licenseLink = renderLicenseLink(license);
+
+  // Return the license section with the license name and link
   return `
 ## License
 This project is licensed under the [${license} License](${licenseLink}).
 `;
 }
 
+// Function to generate the markdown content for the readme file
 function generateMarkdown(data) {
+  // Render the license badge and license section
   const licenseBadge = renderLicenseBadge(data.license);
   const licenseSection = renderLicenseSection(data.license);
 
+  // Generate the markdown content using the provided data
   return `## ${data.title}
 
 ${licenseBadge}
@@ -81,4 +94,5 @@ For any questions or inquiries, please reach out to me via GitHub: [${data.gitHu
 `;
 }
 
+// Export the generateMarkdown function to make it accessible from other files
 module.exports = generateMarkdown;
